89788661c60a0ef60f852cd5f9e42ed8f4d10a11
quanxiao <1940325681@qq.com>
2025-02-26 13:28:34 +0800

    cifir10

--- mytest/Crf10/fedbuffcifir10.py
+++ mytest/Crf10/fedbuffcifir10.py
@@ -5,7 +5,7 @@ import  flgo.algorithm.fedavg as fedavg
 import flgo.benchmark.mnist_classification as mnist
 import flgo.benchmark.cifar10_classification as cifar10
 import flgo.benchmark.partition as fbp
-from flgo.experiment.logger.fedbalance_logger import FedBalanceLogger
+from flgo.experiment.logger.test_logger import TestLogger
 
 task = './cifar10_experiment' # task name
 flgo.gen_task_by_(cifar10, fbp.DirichletPartitioner(num_clients=100, alpha=0.011), task)
@@ -13,5 +13,5 @@ flgo.gen_task_by_(cifar10, fbp.DirichletPartitioner(num_clients=100, alpha=0.011
 
 if __name__ == '__main__':
 
-    fedavg_runner = flgo.init(task=task,Logger=FedBalanceLogger, algorithm=fedbuff, option={'num_rounds': 2500, 'num_epochs': 20, 'cpu': 0,'proportion':0.2},model=cnn)
+    fedavg_runner = flgo.init(task=task,Logger=TestLogger, algorithm=fedbuff, option={'num_rounds': 2500, 'num_epochs': 20, 'cpu': 0,'proportion':0.2},model=cnn)
     fedavg_runner.run()
