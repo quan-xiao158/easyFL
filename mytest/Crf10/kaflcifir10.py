aa94418ba8ac5e6093a595430e8861df75b2321c
quanxiao <1940325681@qq.com>
2025-02-27 00:36:21 +0800

    kafl cifir10

---
+++ mytest/Crf10/kaflcifir10.py
@@ -0,0 +1,15 @@
+import flgo
+import flgo.algorithm.KAFL as  kafl
+import flgo.benchmark.mnist_classification as mnist
+import flgo.benchmark.partition as fbp
+from flgo.experiment.logger.test_logger import TestLogger
+
+
+task = './cifar10_experiment' # task name
+flgo.gen_task_by_(mnist, fbp.DirichletPartitioner(num_clients=100, alpha=0.01), task)
+#100选20客户端
+
+if __name__ == '__main__':
+
+    fedavg_runner = flgo.init(task=task, algorithm=kafl, Logger=TestLogger,option={'num_rounds': 2500, 'num_epochs': 20, 'cpu': 0})
+    fedavg_runner.run()
